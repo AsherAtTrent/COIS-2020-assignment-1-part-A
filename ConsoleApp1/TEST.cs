9da48174b5149b9dcc37e743aa3210460cd1e25b
asher <asherali@trentu.ca>
2022-10-06 18:32:53 -0400

    PARTB: implementation for reverse and equals as well as some new test cases in test

--- ConsoleApp1/TEST.cs
+++ ConsoleApp1/TEST.cs
@@ -1,4 +1,6 @@
-﻿public class assignment1
+﻿using System.Runtime.Intrinsics.Arm;
+
+public class assignment1
 {
     public static void Main()
     {
@@ -11,14 +13,28 @@
             p.PrintGrid();
         }
         else
-        {
+        {      
             char[] a = { 'a','b','c','d'};
             MyString s = new MyString(a);
             s.Print();
-            Console.WriteLine(s.IndexOf('d'));
+            Console.WriteLine(s.IndexOf('a'));
             Console.WriteLine(s.IndexOf('b'));
             Console.WriteLine(s.IndexOf('c'));
-            Console.WriteLine(s.IndexOf('a'));
+            Console.WriteLine(s.IndexOf('d'));
+            s.Reverse();
+            s.Print();
+            s.Reverse();
+            s.Print();
+            MyString test = new MyString(a);
+            char[] b = { 'a','b','c'};
+            char[] c = Array.Empty<char>();
+            char[] d = Array.Empty<char>();
+            MyString test2 = new MyString(b);
+            MyString test3 = new MyString(c);
+            MyString test4 = new MyString(d);
+            Console.WriteLine(s.Equals(test));
+            Console.WriteLine(s.Equals(test2));
+            Console.WriteLine(test3.Equals(test4));
         }
        
 
--- ConsoleApp1/assignment1b.cs
+++ ConsoleApp1/assignment1b.cs
@@ -34,8 +34,20 @@
 
     // Using a stack, reverse this instance of MyString (6 marks)
     public void Reverse() 
-    { 
-        Stack<char> S;
+    {
+        Stack<char> S = new Stack<char>();
+        Node p = front.next;
+        while (p != null)
+        {
+            S.Push(p.item);
+            p = p.next;
+        }
+        p=front.next;
+        while(p != null)
+        {
+            p.item = S.Pop();
+            p = p.next;
+        }
     }
 
 
@@ -68,6 +80,29 @@
     // otherwise false (6 marks)
     public override bool Equals(object obj) 
     {
+
+        if (obj is MyString)
+        {
+            MyString toCompare = (MyString)obj;
+            if(toCompare.length == this.length)
+            {
+                if (toCompare.length == 0){
+                    return true;
+                }
+                Node indx = toCompare.front;
+                Node indy = this.front;
+                while(indx.item == indy.item)
+                {
+                    indx = indx.next;
+                    indy = indy.next;
+                    if(indx == null)
+                    {
+                        return true;
+                    }
+
+                }
+            }
+        }
         return false;
     }
 
